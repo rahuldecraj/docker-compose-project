141cee27121e2235d0f7eef338535d3fe18072e3
RahulRaj <rahuldecraj@gmail.com>
2026-03-27 15:02:10 +0530

    Initial clean commit without binaries

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from prometheus_client import start_http_server, Counter
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('request_count', 'Total Request Count')
+
+@app.route('/')
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello, DevOps World!!!"
+
+if __name__ == '__main__':
+    start_http_server(8000)  # metrics on port 8000
+    app.run(host='0.0.0.0', port=5000)
